c28a7562864063476c2923e964c82de39aec2116
oxalica <oxalicc@pm.me>
2020-01-25 00:31:15 +0800

    Fix executable name of vim

--- nixos/preferences/vim.nix
+++ nixos/preferences/vim.nix
@@ -7,7 +7,7 @@
   nixpkgs.overlays = [
     (self: super: {
       vim-oxa = super.vim_configurable.customize {
-        name = "vim-oxa";
+        name = "vim";
 
         vimrcConfig.packages.myVimPackage = with self.vimPlugins; {
           start = [
