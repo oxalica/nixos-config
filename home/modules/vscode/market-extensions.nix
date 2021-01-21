62cc5279cef0bf4480c22e6e2c34e326466d16c3
oxalica <oxalicc@pm.me>
2021-01-21 23:30:56 +0800

    Add vscode extensions

--- home/modules/vscode/market-extensions.nix
+++ home/modules/vscode/market-extensions.nix
@@ -30,6 +30,12 @@
     sha256 = "0ysbpibzaak9l3i09y0a3haqigb06bfih5dp7mqkih7axaazb6fi";
   })
   (extensionFromVscodeMarketplace {
+    publisher = "dunstontc";
+    name = "viml";
+    version = "0.1.7";
+    sha256 = "0r2dpd07lj8icpbl1nvgk08y6s73bzwv6njpj7m4fcapwjjvhb7l";
+  })
+  (extensionFromVscodeMarketplace {
     publisher = "eamodio";
     name = "gitlens";
     version = "11.1.3";
@@ -64,6 +70,12 @@
     name = "debug";
     version = "0.25.0";
     sha256 = "0qm2jgkj17a0ca5z21xbqzfjpi0hzxw4h8y2hm8c4kk2bnw02sh1";
+  })
+  (extensionFromVscodeMarketplace {
+    publisher = "ziyasal";
+    name = "vscode-open-in-github";
+    version = "1.3.6";
+    sha256 = "156gaj7gcm0588hmwkigkmidf0jxnrq2kvaigf3kszisz05854dq";
   })
   (extensionFromVscodeMarketplace {
     publisher = "zjhmale";
