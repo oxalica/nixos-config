5672d44c384415afe7b154e8862e69f20c33d705
oxalica <oxalicc@pm.me>
2021-01-22 00:05:01 +0800

    Reindent

--- home/modules/shell/cmds.zsh
+++ home/modules/shell/cmds.zsh
@@ -16,25 +16,25 @@ alias t="bsdtar"
 
 # List tree.
 lt() {
-    exa -T --classify --color=always $@ | eval $PAGER
+  exa -T --classify --color=always $@ | eval $PAGER
 }
 
 # Clipboard input/output.
 +() {
-    if ! command -v xsel >/dev/null; then
-        echo "'xsel' not found" >&2
-        return 1
-    elif [[ -t 0 ]]; then # stdin is tty, print clipboard
-        xsel -ob
-    elif [[ -t 1 ]]; then # stdout is tty, put into clipboard
-        xsel -ib
-    else
-        echo "Cannot use '+' as pipe" >&2
-        return 1
-    fi
+  if ! command -v xsel >/dev/null; then
+    echo "'xsel' not found" >&2
+    return 1
+  elif [[ -t 0 ]]; then # stdin is tty, print clipboard
+    xsel -ob
+  elif [[ -t 1 ]]; then # stdout is tty, put into clipboard
+    xsel -ib
+  else
+    echo "Cannot use '+' as pipe" >&2
+    return 1
+  fi
 }
 
 # Realpath of which.
 rwhich() {
-    which $@ | xargs realpath
+  which $@ | xargs realpath
 }
--- home/modules/shell/scripts/flake-prefer-registry.sh
+++ home/modules/shell/scripts/flake-prefer-registry.sh
@@ -6,12 +6,12 @@ registries=( $(nix registry list | sed -nE 's/^\S+\s+flake:(\S+)\s.*$/\1/p' | so
 registries=" ${registries[*]} "
 args=()
 for name in $(nix eval --json -f ./flake.nix inputs | jq -r 'keys | .[]'); do
-    if [[ "$registries" =~ " $name " ]]; then
-        echo "Override $name"
-        args+=(--override-input "$name" "$name")
-    else
-        echo "Free $name"
-    fi
+  if [[ "$registries" =~ " $name " ]]; then
+    echo "Override $name"
+    args+=(--override-input "$name" "$name")
+  else
+    echo "Free $name"
+  fi
 done
 
 exec nix flake update "${args[@]}"
