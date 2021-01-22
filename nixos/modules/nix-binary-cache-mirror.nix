d60a82ade4c4e944f944232b68c87a7c6bc601f6
oxalica <oxalicc@pm.me>
2021-01-22 23:55:31 +0800

    Try sjtu mirror

--- nixos/modules/nix-binary-cache-mirror.nix
+++ nixos/modules/nix-binary-cache-mirror.nix
@@ -1,7 +1,8 @@
 { lib, ... }:
 {
   nix.binaryCaches = lib.mkBefore [
-    "https://mirrors.bfsu.edu.cn/nix-channels/store"
+    "https://mirror.sjtu.edu.cn/nix-channels/store"
+    # "https://mirrors.bfsu.edu.cn/nix-channels/store"
     # Do no try to enable TUNA! Scary thing will happen!
     # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" # May suffer from download stalled issue.
     # "https://mirrors.ustc.edu.cn/nix-channels/store"
