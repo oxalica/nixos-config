715970dfe11639a46816e8adf4d24111b5792f30
oxalica <oxalicc@pm.me>
2021-02-10 21:44:48 +0800

    hm-modules: tweak gpg settings and add `pass`

--- home/modules/gpg.nix
+++ home/modules/gpg.nix
@@ -1,10 +1,24 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.gpg.enable = true;
+  programs.gpg = {
+    enable = true;
+    settings = {
+      default-key = "5CB0E9E5D5D571F57F540FEACED392DE0C483D00";
+      personal-digest-preferences = "SHA256";
+      cert-digest-algo = "SHA256";
+    };
+  };
+
   services.gpg-agent = {
     enable = true;
-    defaultCacheTtl = 1800;
-    enableSshSupport = true;
     pinentryFlavor = "qt";
+    # enableSshSupport = true;
+    defaultCacheTtl = 12 * 3600;
+    maxCacheTtl = 24 * 3600;
+  };
+
+  programs.password-store = {
+    enable = true;
+    package = pkgs.pass;
   };
 }
