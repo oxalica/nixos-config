966a64159e176100184b88ea22cb25d58fc56150
oxalica <oxalicc@pm.me>
2021-02-14 02:44:20 +0800

    silver: change to nixos-20.09

--- nixos/hosts/silver/default.nix
+++ nixos/hosts/silver/default.nix
@@ -1,8 +1,8 @@
 { inputs, overlays, ... }:
-inputs.nixpkgs.lib.nixosSystem {
+inputs.nixpkgs-stable.lib.nixosSystem {
   system = "x86_64-linux";
   modules = [
     ./configuration.nix
   ];
-  specialArgs.inputs = inputs // { nixpkgs = inputs.nixpkgs; };
+  specialArgs.inputs = inputs // { nixpkgs = inputs.nixpkgs-stable; };
 }
