247684e735bf34375114f3ab2017730b365a5ef7
oxalica <oxalicc@pm.me>
2021-05-02 01:27:55 +0800

    rust: use ld.lld

--- home/modules/rust.nix
+++ home/modules/rust.nix
@@ -8,6 +8,36 @@ let
 
   home = config.home.homeDirectory;
 
+  cargoConfig = ''
+    [install]
+    root = "${home}/.local"
+
+    [build]
+    target-dir = "${targetDir}"
+
+    ${lib.optionalString enableLld ''
+      [target."${rustHostTarget}"]
+      linker = "${lld-linker}"
+    ''}
+  '';
+
+  enableLld = true;
+  rustHostTarget = pkgs.rust.toRustTarget pkgs.stdenv.hostPlatform;
+  lld-wrapper = pkgs.wrapBintoolsWith {
+    bintools = pkgs.writeShellScriptBin "ld.lld" ''
+      set -e
+      exec -a ld.lld "$(rustc --print sysroot)/lib/rustlib/${rustHostTarget}/bin/rust-lld" "$@"
+    '';
+  };
+  lld-linker = pkgs.writeShellScript "lld-linker" ''
+    if [[ -z "$RUST_NO_LLD" ]]; then
+      export PATH="${lld-wrapper}/bin''${PATH:+:}$PATH"
+      exec ${pkgs.gcc}/bin/gcc -fuse-ld=lld "$@"
+    else
+      exec ${pkgs.gcc}/bin/gcc "$@"
+    fi
+  '';
+
 in {
   home.packages = with pkgs; [
     (rust-bin.stable.latest.default.override {
@@ -25,33 +55,17 @@ in {
     cargo-insta
   ];
 
-  /*
-    triple = pkgs.stdenv.hostPlatform.config;
-    rust-lld-wrapper = pkgs.writeShellScriptBin "ld.lld" ''
-      set -e
-      exec -a ld.lld "$(rustc --print sysroot)/lib/rustlib/${triple}/bin/rust-lld" "$@"
-    '';
-    linker = pkgs.writeShellScript "rust-ld-wrapper" ''
-      export PATH="${rust-lld-wrapper}/bin''${PATH:+:}$PATH"
-      exec ${pkgs.gcc}/bin/gcc -fuse-ld=lld "$@"
-    '';
-  */
-
   # Setup cargo directories.
   # https://doc.rust-lang.org/cargo/commands/cargo.html?highlight=cargo_home#files
-  home.sessionVariables."CARGO_HOME" = pkgs.runCommand "cargo-home" {} ''
+  home.sessionVariables."CARGO_HOME" = pkgs.runCommandLocal "cargo-home" {
+    inherit cargoConfig;
+  } ''
     mkdir -p $out
     ln -st $out "${home}"/.cache/cargo/{registry,git}
     ln -st $out "${home}"/.config/cargo/credentials.toml
-
-    cat >$out/config.toml <<EOF
-    [install]
-    root = "${home}/.local"
-
-    [build]
-    target-dir = "${targetDir}"
-    EOF
+    echo -n "$cargoConfig" >$out/config.toml
   '';
+
   home.activation.setupCargoDirectories = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     $DRY_RUN_CMD mkdir -p "${home}"/{.config/cargo,.cache/cargo/{registry,git}}
     $DRY_RUN_CMD touch -a "${home}"/.config/cargo/credentials.toml
