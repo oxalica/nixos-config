a4849018311f36c8291ced0dd5ce226e368d7411
oxalica <oxalicc@pm.me>
2021-05-28 01:03:44 +0800

    tmux: reconfigure

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -5,38 +5,61 @@
     historyLimit = 5000;
     keyMode = "vi";
     terminal = "screen-256color"; # $TERM
+    prefix = "C-a";
+    clock24 = true;
 
     extraConfig = ''
-      set -g prefix C-a
       set -g mouse on
 
-      bind r source-file ~/.config/tmux/tmux.conf
+      # Reload.
+      bind-key r source-file ~/.config/tmux/tmux.conf \; \
+        display-message "source-file done"
 
-      # Copy mode
-      bind -n -T copy-mode-vi v send-keys -X begin-selection
-      bind -n -T copy-mode-vi y send-keys -X copy-pipe "xsel -ib"
+      # Split panes.
+      bind-key v split-window -h -c "#{pane_current_path}"
+      bind-key s split-window -v -c "#{pane_current_path}"
 
-      # Split panes
-      bind v split-window -h
-      bind s split-window -v
+      # Resize panes.
+      bind-key -r H resize-pane -L 5
+      bind-key -r J resize-pane -D 5
+      bind-key -r K resize-pane -U 5
+      bind-key -r L resize-pane -R 5
 
-      # Resize panes
-      bind -r < resize-pane -L 5
-      bind -r + resize-pane -D 5
-      bind -r - resize-pane -U 5
-      bind -r > resize-pane -R 5
+      # Move between panes.
+      bind-key -r C-h select-pane -L
+      bind-key -r C-j select-pane -D
+      bind-key -r C-k select-pane -U
+      bind-key -r C-l select-pane -R
 
-      # Move between panes
-      bind -r C-h select-pane -L
-      bind -r C-j select-pane -D
-      bind -r C-k select-pane -U
-      bind -r C-l select-pane -R
+      # Copy & paste.
+      set-option -s set-clipboard off
+      # Copy mode.
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi V send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "xsel -ib"
+      bind-key -T copy-mode-vi MouseDragEnd1Pane send-keys -X copy-pipe-no-clear "xsel -ip"
+      bind-key -T copy-mode-vi DoubleClick1Pane send-keys -X select-word \; \
+        send-keys -X copy-pipe-no-clear "xsel -ip"
+      bind-key -T copy-mode-vi TripleClick1Pane send-keys -X select-line \; \
+        send-keys -X copy-pipe-no-clear "xsel -ip"
+
+      # Normal mode copy.
+      bind-key -n DoubleClick1Pane select-pane \; copy-mode -M \; send-keys -X select-word \; \
+        send-keys -X copy-pipe-no-clear "xsel -ip"
+      bind-key -n TripleClick1Pane select-pane \; copy-mode -M \; send-keys -X select-line \; \
+        send-keys -X copy-pipe-no-clear "xsel -ip"
+
+      # Copy & normal mode paste.
+      bind-key -T copy-mode-vi MouseDown2Pane send-keys -X cancel \; \
+        run "tmux set-buffer -b primary_selection \"$(xsel -op)\"; tmux paste-buffer -b primary_selection"
+      bind-key -n MouseDown2Pane select-pane \; \
+        run "tmux set-buffer -b primary_selection \"$(xsel -op)\"; tmux paste-buffer -b primary_selection"
 
       # Clear some default bindings.
-      unbind up
-      unbind down
-      unbind left
-      unbind right
+      unbind-key up
+      unbind-key down
+      unbind-key left
+      unbind-key right
     '';
   };
 }
