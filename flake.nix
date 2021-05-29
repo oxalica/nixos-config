a76bb0928f519ff5d5efe4bc83894ff88829f921
oxalica <oxalicc@pm.me>
2021-05-29 22:53:32 +0800

    invar: use old firmware

--- flake.nix
+++ flake.nix
@@ -5,6 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-20.09";
 
+    # `amdgpu` causes GPU reset when using firefox hardware decoding, in current unstable.
+    # Checkout old firmware for test.
+    # 11 May 00:20 system-128-link -> /nix/store/r5275bdswf52h02zshqx3v448ghp959x-nixos-system-invar-21.05.20210506.6358647/
+    # 20 May 18:50 system-129-link -> /nix/store/7x4hk9mwzi18nx4x0zx61x4rysmkdb8c-nixos-system-invar-21.05.20210516.7a1fbc3/ <- seems the first bad
+    # 24 May 15:26 system-130-link -> /nix/store/b44fr7qy02yp0c65k30ra7lbxcnh4h2r-nixos-system-invar-21.05.20210523.900115a/
+    # 25 May 01:14 system-131-link -> /nix/store/zyb0s5kjpykfa7gnkwq922svkw0m4mdl-nixos-system-invar-21.05.20210523.900115a/
+    # 25 May 20:31 system-132-link -> /nix/store/jypydm0v61y6jl8bv20ciix0rkdf5gkj-nixos-system-invar-21.05.20210523.900115a/
+    nixpkgs-old-firmware.url = "github:nixos/nixpkgs/63586475587d7e0e078291ad4b49b6f6a6885100";
+
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -54,6 +63,10 @@
           patches = (oldAttrs.patches or []) ++
             [ ./patches/tdesktop-0001-use-system-font-and-use-stylename.patch ];
         });
+      };
+
+      old-firmware = final: prev: {
+        inherit (inputs.nixpkgs-old-firmware.legacyPackages.${final.system}) firmwareLinuxNonfree;
       };
     };
 
--- nixos/hosts/invar/default.nix
+++ nixos/hosts/invar/default.nix
@@ -3,7 +3,7 @@ inputs.nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
   modules = [
     inputs.home-manager.nixosModules.home-manager
-    { nixpkgs.overlays = with overlays; [ rust-overlay xdgify-overlay tdesktop-font ]; }
+    { nixpkgs.overlays = with overlays; [ rust-overlay xdgify-overlay tdesktop-font old-firmware ]; }
     ./configuration.nix
     ({ lib, ... }: {
       options.home-manager.users = with lib.types; lib.mkOption {
