9ca91cb437a4a0fee9867e8f55a11b3f46d5e746
oxalica <oxalicc@pm.me>
2021-05-31 02:47:42 +0800

    console-env: tweak `less` options

--- nixos/modules/console-env.nix
+++ nixos/modules/console-env.nix
@@ -3,7 +3,13 @@
   programs.vim.defaultEditor = true;
 
   programs.less.enable = true;
-  programs.less.envVariables.LESS = "--RAW-CONTROL-CHARS --quit-if-one-screen --mouse";
+  programs.less.envVariables.LESS = lib.concatStringsSep " " [
+    "--RAW-CONTROL-CHARS"
+    "--quit-if-one-screen"
+    "--mouse"
+    "--wheel-lines=5"
+    "--no-init" # Don't clear on exit.
+  ];
 
   programs.iotop.enable = true;
   programs.iftop.enable = true;
