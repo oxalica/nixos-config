3f38eebe78fee29829627738edbe1b4f2219adf6
oxalica <oxalicc@pm.me>
2021-07-23 05:04:40 +0800

    flake: update

--- flake.nix
+++ flake.nix
@@ -52,32 +52,6 @@
       rust-overlay = inputs.rust-overlay.overlay;
       xdgify-overlay = inputs.xdgify-overlay.overlay;
       tdesktop-voice-fix = prToOverlay inputs.nixpkgs-tdesktop-voice-fix [ "tdesktop" ];
-
-      # https://github.com/NixOS/nixpkgs/pull/130840
-      skip-osu = final: prev: {
-        osu-lazer = prev.hello;
-      };
-    };
-
-    # Temporary fix for https://github.com/NixOS/nixpkgs/issues/128893.
-    config-alsa-1-2-5-1 = { lib, pkgs, ... }: {
-      system.replaceRuntimeDependencies = lib.singleton {
-        original = pkgs.alsa-lib;
-        replacement = pkgs.alsa-lib.overrideAttrs (drv: {
-          # NOTES:
-          #
-          # Since the store paths are replaced in the system closure, we can't use
-          # "1.2.5.1" here because it would result in a different length.
-          #
-          # Additionally, the assertion here is to make sure that once version
-          # 1.2.5.1 hits the system we get an error and can remove this altogether.
-          version = assert pkgs.alsa-lib.version == "1.2.5"; "1.2.X";
-          src = pkgs.fetchurl {
-            url = "mirror://alsa/lib/${drv.pname}-1.2.5.1.tar.bz2";
-            hash = "sha256-YoQh2VDOyvI03j+JnVIMCmkjMTyWStdR/6wIHfMxQ44=";
-          };
-        });
-      };
     };
 
     config-nixos-tag = { config, lib, pkgs, ... }: let
@@ -129,8 +103,8 @@
 
     } // {
       invar = mkSystem "x86_64-linux"
-        (with overlays; [ rust-overlay xdgify-overlay tdesktop-voice-fix skip-osu ])
-        [ ./nixos/hosts/invar/configuration.nix config-alsa-1-2-5-1 config-nixos-tag ];
+        (with overlays; [ rust-overlay xdgify-overlay tdesktop-voice-fix ])
+        [ ./nixos/hosts/invar/configuration.nix config-nixos-tag ];
 
       blacksteel = mkSystem "x86_64-linux"
         (with overlays; [ rust-overlay ])
