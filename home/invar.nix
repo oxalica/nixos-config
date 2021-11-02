878ad54ce397012704350255ca8954a703c7ed45
oxalica <oxalicc@pm.me>
2021-11-03 05:15:42 +0800

    {invar,blacksteel,silver}: update stateVersion to 21.05

--- home/invar.nix
+++ home/invar.nix
@@ -64,5 +64,5 @@
   # You can update Home Manager without changing this value. See
   # the Home Manager release notes for a list of state version
   # changes in each release.
-  home.stateVersion = "20.03";
+  home.stateVersion = "21.05";
 }
--- nixos/blacksteel/configuration.nix
+++ nixos/blacksteel/configuration.nix
@@ -45,5 +45,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
+  system.stateVersion = "21.05"; # Did you read the comment?
 }
