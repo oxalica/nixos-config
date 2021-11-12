ef465225bdf68e3ba26932fa7cc70ecc16782173
oxalica <oxalicc@pm.me>
2021-11-12 13:14:21 +0800

    invar: move pipewire 32bit option to steam-compat

--- nixos/invar/system.nix
+++ nixos/invar/system.nix
@@ -19,7 +19,6 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
   };
 }
--- nixos/modules/steam-compat.nix
+++ nixos/modules/steam-compat.nix
@@ -3,4 +3,5 @@
 {
   hardware.opengl.driSupport32Bit = true;
   hardware.pulseaudio.support32Bit = true;
+  services.pipewire.alsa.support32Bit = true;
 }
