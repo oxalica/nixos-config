3e9ade8cf62efaa20aec810092a1a7cb905d265f
oxalica <oxalicc@pm.me>
2021-11-26 01:18:00 +0800

    stretchly: disable auto-start due to a critical bug

--- home/modules/stretchly.nix
+++ home/modules/stretchly.nix
@@ -4,6 +4,8 @@
     pkgs.stretchly
   ];
 
-  xdg.configFile."autostart/stretchly.desktop".source =
-    "${pkgs.stretchly}/share/applications/stretchly.desktop";
+  # FIXME: Enable when bumped to 1.8.
+  # https://github.com/NixOS/nixpkgs/pull/147374
+  # xdg.configFile."autostart/stretchly.desktop".source =
+  #   "${pkgs.stretchly}/share/applications/stretchly.desktop";
 }
