d387c5e4a2e30f0f2d9b620036501821185a7c2c
oxalica <oxalicc@pm.me>
2021-12-10 21:15:40 +0800

    nix: switch to bfsu mirror

--- nixos/modules/nix-binary-cache-mirror.nix
+++ nixos/modules/nix-binary-cache-mirror.nix
@@ -1,8 +1,9 @@
 { lib, ... }:
 {
   nix.binaryCaches = lib.mkBefore [
-    "https://mirror.sjtu.edu.cn/nix-channels/store"
-    # "https://mirrors.bfsu.edu.cn/nix-channels/store"
+    # "https://mirror.sjtu.edu.cn/nix-channels/store" # Frequent download stall.
+    "https://mirrors.bfsu.edu.cn/nix-channels/store"
+
     # Do no try to enable TUNA! Scary thing will happen!
     # "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store" # May suffer from download stalled issue.
     # "https://mirrors.ustc.edu.cn/nix-channels/store"
