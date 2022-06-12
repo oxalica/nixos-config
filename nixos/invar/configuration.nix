80ee3a903474a86f46bdb5dedc5ef308caed51c9
oxalica <oxalicc@pm.me>
2022-06-13 06:48:19 +0800

    invar: unify configurations

--- nixos/invar/configuration.nix
+++ nixos/invar/configuration.nix
@@ -1,28 +1,96 @@
-{ lib, config, pkgs, inputs, ... }:
-
+{ lib, config, pkgs, inputs, my, ... }:
 {
   imports = [
-    ./boot.nix
     ./btrbk.nix
-    ./software.nix
-    ./system.nix
 
     ../modules/console-env.nix
     ../modules/nix-binary-cache-mirror.nix
     ../modules/nix-common.nix
     ../modules/nix-registry.nix
-    ../modules/nixpkgs-allow-unfree-list.nix
     ../modules/steam-compat.nix
     ../modules/sway-desktop.nix
-    ../modules/user-oxa.nix
   ] ++ lib.optional (inputs ? secrets) inputs.secrets.nixosModules.invar;
 
   sops.age.sshKeyPaths = lib.mkForce [ "/var/ssh/ssh_host_ed25519_key" ];
 
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes ca-derivations
-  '';
+  nixpkgs.config.allowUnfreePredicate =
+    drv: lib.elem (lib.getName drv) [
+      "steam" "steam-original"
+    ];
 
+  # Boot.
+
+  boot = {
+    initrd = {
+      availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" ];
+      kernelModules = [ "dm-snapshot" ];
+      luks.devices."unluks" = {
+        device = "/dev/disk/by-uuid/21764e86-fde3-4e51-9652-da9adbdeeb34";
+        preLVM = true;
+        allowDiscards = true;
+      };
+    };
+
+    kernelModules = [
+      "kvm-amd"
+      "nct6775" # Fan control
+    ];
+    extraModulePackages = [ ];
+
+    kernel.sysctl = {
+      "net.ipv4.ip_forward" = 1;
+      "kernel.sysrq" = 1;
+      "net.ipv4.tcp_congestion_control" = "bbr";
+    };
+
+    loader = {
+      systemd-boot.enable = true;
+      systemd-boot.consoleMode = "max"; # Don't clip boot menu.
+      efi.canTouchEfiVariables = false;
+      timeout = 1;
+    };
+
+    # For dev.
+    binfmt.emulatedSystems = [ "riscv64-linux" ];
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-uuid/7219f4b1-a9d1-42a4-bfc9-386fa919d44b";
+      fsType = "btrfs";
+      # zstd:1  W: ~510MiB/s
+      # zstd:3  W: ~330MiB/s
+      options = [ "compress-force=zstd:1" "noatime" "subvol=/@" ];
+    };
+
+    "/boot" = {
+      device = "/dev/disk/by-uuid/DDBD-2F2B";
+      fsType = "vfat";
+    };
+  };
+
+  swapDevices = [
+    {
+      device = "/var/swapfile";
+      # FIXME: Auto creation sucks on btrfs.
+      # size = 16 * 1024; # 16G
+    }
+  ];
+
+  # Hardware.
+
+  time.timeZone = "Asia/Shanghai";
+  powerManagement.cpuFreqGovernor = "ondemand";
+  hardware = {
+    enableRedistributableFirmware = true;
+    video.hidpi.enable = true;
+    cpu.amd.updateMicrocode = true;
+    bluetooth.enable = true;
+  };
+  console = {
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-v28n.psf.gz";
+    useXkbConfig = true;
+  };
   networking = {
     hostName = "invar";
     search = [ "lan." ];
@@ -42,29 +110,123 @@
     timeout = 15;
   };
 
-  time.timeZone = "Asia/Shanghai";
+  # Users.
 
   sops.secrets.passwd.neededForUsers = true;
+  programs.zsh.enable = true;
   users = {
     mutableUsers = false;
     users."oxa" = {
+      isNormalUser = true;
       shell = pkgs.zsh;
       passwordFile = config.sops.secrets.passwd.path;
+      uid = 1000;
+      group = config.users.groups.oxa.name;
+      extraGroups = [ "wheel" "libvirtd" ];
     };
+    groups."oxa".gid = 1000;
+  };
+  home-manager.users."oxa" = import ../../home/invar.nix;
+
+  # Services.
+
+  security.rtkit.enable = true; # Better installed with pipewire.
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
   };
 
-  home-manager.users."oxa" = import ../../home/invar.nix;
+  services.xserver.xkbOptions = "ctrl:swapcaps";
+
+  services.timesyncd.enable = true;
+
+  services.fstrim = {
+    enable = true;
+    interval = "Sat";
+  };
 
   services.logind.extraConfig = ''
     HandlePowerKey=suspend
   '';
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.11"; # Did you read the comment?
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    kbdInteractiveAuthentication = false;
+    permitRootLogin = "no";
+    hostKeys = [
+      {
+        type = "rsa";
+        path = "/var/ssh/ssh_host_rsa_key";
+        bits = 4096;
+      }
+      {
+        type = "ed25519";
+        path = "/var/ssh/ssh_host_ed25519_key";
+        rounds = 100;
+      }
+    ];
+  };
 
+  services.earlyoom = {
+    enable = true;
+    freeMemThreshold = 5;
+    freeSwapThreshold = 10;
+    enableNotifications = true;
+  };
+
+  services.udev.packages = with pkgs; [ logitech-udev-rules ];
+
+  services.transmission = {
+    enable = true;
+    home = "/home/transmission";
+  };
+  users.groups."transmission".members = [ config.users.users.oxa.name ];
+
+  virtualisation.libvirtd = {
+    enable = true;
+    onBoot = "ignore";
+  };
+
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes ca-derivations
+  '';
+
+  # Global ssh settings. Also for remote builders.
+  programs.ssh = {
+    knownHosts = my.ssh.knownHosts;
+    extraConfig = ''
+      Include ${config.sops.secrets.ssh-hosts.path}
+    '';
+  };
+  sops.secrets.ssh-hosts = {
+    sopsFile = ../../secrets/ssh.yaml;
+    mode = "0444";
+  };
+
+  programs.mtr.enable = true;
+
+  programs.adb.enable = true;
+  users.groups."adbusers".members = [ config.users.users.oxa.name ];
+
+  environment.etc = {
+    "machine-id".source = "/var/machine-id";
+    "ssh/ssh_host_rsa_key".source = "/var/ssh/ssh_host_rsa_key";
+    "ssh/ssh_host_rsa_key.pub".source = "/var/ssh/ssh_host_rsa_key.pub";
+    "ssh/ssh_host_ed25519_key".source = "/var/ssh/ssh_host_ed25519_key";
+    "ssh/ssh_host_ed25519_key.pub".source = "/var/ssh/ssh_host_ed25519_key.pub";
+  };
+
+  environment.systemPackages = with pkgs; [
+    cntr # Debug nix build.
+    curl
+    git
+    virt-manager
+    solaar # Logitech devices control.
+    rawmv # Subvolume operations.
+    compsize
+  ];
+
+  system.stateVersion = "21.11";
 }
--- nixos/modules/user-oxa.nix
+++ nixos/modules/user-oxa.nix
@@ -1,10 +1,3 @@
 { config, ... }:
 {
-  users.users."oxa" = {
-    isNormalUser = true;
-    uid = 1000;
-    group = "oxa";
-    extraGroups = [ "wheel" ];
-  };
-  users.groups."oxa".gid = 1000;
 }
