0a798f847d2e3d92670824b0b9754f68cd5aed27
oxalica <oxalicc@pm.me>
2022-06-26 02:29:24 +0800

    tmux: tweak and simplify

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -2,46 +2,28 @@
 {
   programs.tmux = {
     enable = true;
-    historyLimit = 5000;
-    keyMode = "vi";
-    terminal = "tmux-256color"; # Fix italic and true color support.
-    prefix = "C-a";
+    baseIndex = 1;
     clock24 = true;
+    escapeTime = 1;
+    historyLimit = 10000;
+    prefix = "C-a";
+    terminal = "tmux";
 
     # tmux
     extraConfig = ''
       set -g mouse on
-      set -g escape-time 1 # Don't mess up when Esc followed by some keys.
       set -g set-clipboard on
 
-      # Fixups
-      # For true colors
-      # https://gist.github.com/XVilka/8346728
       set -sa terminal-overrides "alacritty:Tc"
-      set -sa terminal-overrides "xterm-256color:Tc"
-      # hyperlink (experimental)
-      set -sa terminal-overrides '*:Hls=\E]8;id=%p1%s;%p2%s\E\\:Hlr=\E]8;;\E\\'
-      # SGR 53 (overline)
-      set -sa terminal-overrides '*:Smol=\E[53m'
-      # styled underscore
-      set -sa terminal-overrides '*:Smulx=\E[4::%p1%dm'
-      # underscore colors
-      set -sa terminal-overrides '*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
 
       # Colors
-      set -g status-style fg=yellow
-      set -g window-status-current-style bg=yellow,fg=black
-      set -g message-style bg=black,fg=yellow
-      set -g message-command-style bg=yellow,fg=black
-      set -g pane-active-border-style fg=magenta
+      set -g status-style fg=green
+      set -g window-status-current-style reverse
 
-      # Custom window title
+      # Custom window title.
       set -g automatic-rename on
-      set -g automatic-rename-format '#{b:pane_current_path}:#{?#{!=:#{window_panes},1},#{window_panes}:,}#{pane_current_command}'
-
-      # Reload.
-      bind r source-file ~/.config/tmux/tmux.conf \; \
-        display-message "source-file done"
+      set -g automatic-rename-format '#{b:pane_current_path}#{?#{!=:#{pane_current_command},zsh},:#{pane_current_command},}'
+      set -g status-interval 1
 
       # Split panes.
       bind v split-window -h -c "#{pane_current_path}"
@@ -58,12 +40,6 @@
       bind -r C-j select-pane -D
       bind -r C-k select-pane -U
       bind -r C-l select-pane -R
-
-      # Clear some default bindings.
-      unbind up
-      unbind down
-      unbind left
-      unbind right
     '';
   };
 }
