2cdc3dd044e74df3ca522f0d7906d36ada5fff3f
oxalica <oxalicc@pm.me>
2022-10-24 11:24:33 +0800

    sway: use `greetd` login manager

--- nixos/modules/sway-desktop.nix
+++ nixos/modules/sway-desktop.nix
@@ -8,4 +8,18 @@
 
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ]; # `wlr` is included by `programs.sway`.
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session.command = "${pkgs.greetd.tuigreet}/bin/tuigreet --cmd sway";
+    };
+  };
+
+  # Disables systemd messages in greeter.
+  # Ref: https://github.com/NickCao/flakes/blob/4beaae04bbc89fd377b390266788c05356350808/nixos/local/configuration.nix#L176
+  systemd.services.greetd.serviceConfig = {
+    ExecStartPre = "${pkgs.util-linux}/bin/kill -SIGRTMIN+21 1";
+    ExecStopPost = "${pkgs.util-linux}/bin/kill -SIGRTMIN+20 1";
+  };
 }
