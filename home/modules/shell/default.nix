efb3868f51100d61c3fc4ba16d40b26f83ca9cfa
oxalica <oxalicc@pm.me>
2022-11-09 00:58:08 +0800

    shell: disable extended_glob to fix nix flake path

--- home/modules/shell/default.nix
+++ home/modules/shell/default.nix
@@ -57,10 +57,10 @@
     # bash
     initExtra = ''
       setopt auto_pushd
-      setopt extended_glob
       setopt hist_verify
       setopt interactive_comments
       setopt multios
+      setopt noextended_glob # Breaks flake path reference nixpkgs#foo.
       export LS_COLORS='rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:'
       TIMEFMT=$'%J  %uU user %uS system %uE/%*E elapsed %PCPU (%Xavgtext+%Davgdata %Mmaxresident)k\n%Iinputs+%Ooutputs (%Fmajor+%Rminor)pagefaults %Wswaps'
 
