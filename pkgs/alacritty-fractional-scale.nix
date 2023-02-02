93ca9422e2b6ccf8b4224987b265d622d1530483
oxalica <oxalicc@pm.me>
2023-02-02 17:19:14 +0800

    alacritty-fractional-scale: update

--- pkgs/alacritty-fractional-scale.nix
+++ pkgs/alacritty-fractional-scale.nix
@@ -3,14 +3,13 @@ alacritty.overrideAttrs (old: rec {
   version = "fractional-scale";
 
   src = fetchFromGitHub {
-    owner = "oxalica";
-    repo = old.pname;
-    rev = "a473de8fb83a3429d31b33db4b971ea09fa2629a";
-    hash = "sha256-uGhHUmbYBbdeh465lx6P1i+nSivneMSBLDtkWvMTF2w=";
+    inherit (old.src) owner repo;
+    rev = "3354203e571e427b7152999d13af8a1cbd14e0d8";
+    hash = "sha256-KxslwPEjVvd6KWWwPAFra+aSCMdHaFX/GQyHyrYGrRs=";
   };
 
   cargoDeps = rustPlatform.fetchCargoTarball {
     inherit src;
-    hash = "sha256-3xe2vxwSSm+vq/i5ldCK6/sqz8JGwMKDb7gBqoXy7x0=";
+    hash = "sha256-NBAI4Qix4vRE1CIRizEXl9iIjfZpIyNKqUfgQLnZUpg=";
   };
 })
