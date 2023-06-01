6e9d2c404e44dbe676ec72d5ae751580b9df4e12
oxalica <oxalicc@pm.me>
2023-06-01 15:41:52 +0800

    flake: update to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-23.05";
     nixpkgs-unmatched.url = "github:oxalica/nixpkgs/test/unmatched";
 
     # Placeholder.
