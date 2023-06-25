cd627d49238e143ab709f10135c5b7f85dd03c91
oxalica <oxalicc@pm.me>
2023-06-25 17:25:43 +0800

    sway-git-unwrapped: apply IME popup patch

--- pkgs/sway-git-unwrapped.nix
+++ pkgs/sway-git-unwrapped.nix
@@ -3,6 +3,7 @@
   source,
   sway-unwrapped,
   wlroots-git,
+  fetchpatch,
 
   isNixOS ? false,
   enableXWayland ? true
@@ -14,5 +15,13 @@
   inherit (source) src;
   version = "git-${source.date}";
 
-  patches = lib.filter (p: p.name or "" != "LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.patch") old.patches;
+  patches =
+    lib.filter (p: p.name or "" != "LIBINPUT_CONFIG_ACCEL_PROFILE_CUSTOM.patch") old.patches ++ [
+      # text_input: Implement input-method popups
+      # https://github.com/swaywm/sway/pull/7226
+      (fetchpatch {
+        url = "https://github.com/swaywm/sway/pull/7226/commits/53d4cf1b93de202da22b7f2132e2086eace63ff6.patch";
+        hash = "sha256-SZj7W8Iji7KBxBSsj+DKQb64dedEwrvHeMNhNDtNNAs=";
+      })
+    ];
 })
