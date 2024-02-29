b9504677d1d63aaf9578df840bc70804f88a6b25
oxalica <oxalicc@pm.me>
2024-02-29 03:17:49 -0500

    blacksteel: switch to plasma 6

--- nixos/modules/kde-desktop/default.nix
+++ nixos/modules/kde-desktop/default.nix
@@ -2,39 +2,23 @@
 {
   imports = [ ../l10n.nix ];
 
-  environment.systemPackages = with pkgs.plasma5Packages; [
-    ark
+  environment.systemPackages = with pkgs.kdePackages; [
     filelight
-    plasma-browser-integration
     my.pkgs.bismuth-fix-5-27
   ];
 
   programs = {
     partition-manager.enable = true;
     kdeconnect.enable = true;
-    dconf.enable = true;
   };
-
-  nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
 
   services.xserver = {
     enable = true;
     xkb.layout = "us";
 
-    displayManager = {
-      sddm.enable = true;
-      defaultSession = "plasmawayland";
-    };
-
-    desktopManager.plasma5 = {
-      enable = true;
-      runUsingSystemd = true;
-
-      kdeglobals.KDE.SingleClick = false;
-    };
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
   };
-
-  security.pam.services.sddm.enableKwallet = true;
 
   networking.networkmanager.enable = true;
 }
