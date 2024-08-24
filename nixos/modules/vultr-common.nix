b7861e4e7ae98b1ce311e72e5395902d0f57878b
oxalica <oxalicc@pm.me>
2024-08-24 06:44:23 -0400

    vultr-common: more micro optimizaion

--- nixos/modules/vultr-common.nix
+++ nixos/modules/vultr-common.nix
@@ -1,7 +1,7 @@
 { lib, config, pkgs, modulesPath, my, ... }:
 {
   imports = [
-    (modulesPath + "/profiles/qemu-guest.nix")
+    "${modulesPath}/profiles/qemu-guest.nix"
 
     ../modules/zswap-enable.nix
     ./vultr-image.nix
@@ -16,6 +16,10 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
+  boot.kernel.sysctl = {
+    "net.ipv4.tcp_congestion_control" = "bbr";
+  };
+
   fileSystems."/" = {
     device = "/dev/disk/by-label/nixos";
     fsType = "ext4";
@@ -24,11 +28,12 @@
   # The initial partition is very small. Do not enable swap on it, or the system will freeze.
   swapDevices = [ ];
 
+  networking.useNetworkd = true;
   networking.useDHCP = false;
   networking.interfaces.enp1s0.useDHCP = true;
   networking.firewall.enable = false; # Already have a hardware firewall.
   networking.nameservers = [
-    # "1.1.1.1" "1.0.0.1"
+    "1.1.1.1" "1.0.0.1"
     "2606:4700:4700::1111" "2606:4700:4700::1001"
   ];
 
@@ -50,7 +55,12 @@
   security.sudo.wheelNeedsPassword = false;
 
   nix.package = pkgs.nix;
+  nix.gc.options = lib.mkForce "--delete-older-than 3d";
   nix.settings.auto-optimise-store = lib.mkForce false;
+  # Avoid dependency to nixpkgs itself.
+  nix.settings.nix-path = lib.mkForce "";
+  nix.registry = lib.mkForce { };
+  nix.nixPath = lib.mkForce [ ];
 
   services.openssh = {
     enable = true;
