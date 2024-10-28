52bc427c5160a27ca63060dab5b7d394bba931d6
oxalica <oxalicc@pm.me>
2024-10-28 16:58:19 -0400

    invar: passthru `gamemoderun` into steam env

--- nixos/invar/configuration.nix
+++ nixos/invar/configuration.nix
@@ -260,6 +260,7 @@
   users.groups."adbusers".members = [ config.users.users.oxa.name ];
 
   programs.steam.enable = true;
+  programs.steam.extraPackages = [ pkgs.gamemode ];
   programs.gamemode = {
     enable = true;
     settings = {
