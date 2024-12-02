a5e359690c634b6ea03d26e7c80ee172139c0d6e
oxalica <oxalicc@pm.me>
2024-12-02 08:52:38 -0500

    console-env: switch from gdu to ncdu
    It gets `-x` implementation wrong and is incompatible with BTRFS
    subvols.

--- nixos/modules/console-env.nix
+++ nixos/modules/console-env.nix
@@ -13,7 +13,7 @@
 
   environment.systemPackages = with pkgs; [
     cntr # Nix helpers.
-    procs gdu swapview smartmontools pciutils usbutils # System info.
+    procs ncdu swapview smartmontools pciutils usbutils # System info.
     moreutils curl git strace pv tree fd ripgrep lsof jq loop bc file rsync dnsutils # Utilities.
     compsize e2fsprogs # Filesystems.
     gnupg age pwgen sops ssh-to-age # Crypto.
