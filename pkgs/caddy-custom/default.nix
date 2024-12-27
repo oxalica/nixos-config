9649cc68bf5e1e63aea17b3b350b6f6968cc9c7d
oxalica <oxalicc@pm.me>
2024-12-27 15:31:22 -0500

    caddy-custom: use withPlugins instead of a fork

--- pkgs/caddy-custom/default.nix
+++ pkgs/caddy-custom/default.nix
@@ -1,48 +1,24 @@
 {
-  source,
-  lib,
-  buildGoModule,
-  fetchFromGitHub,
+  caddy,
+  fetchpatch,
 }:
 let
-  version = "2.8.4";
-  dist = fetchFromGitHub {
-    owner = "caddyserver";
-    repo = "dist";
-    rev = "v${version}";
-    hash = "sha256-O4s7PhSUTXoNEIi+zYASx8AgClMC5rs7se863G6w+l0=";
+  caddy' = caddy.overrideAttrs (old: {
+    patches = old.patches or [ ] ++ [
+      # caddyauth: use same cost for users and fake hash
+      (fetchpatch {
+        url = "https://github.com/oxalica/caddy/commit/b7a5e89fa55075cb2522a4185e730c7c1f3768b6.patch";
+        hash = "sha256-A01EbLJRvzDxpPyWm4TKlcUT+SsmaSzQ9q+cvZzIIG4=";
+      })
+    ];
+  });
+
+  caddy'' = caddy'.withPlugins {
+    plugins = [
+      "github.com/mholt/caddy-webdav@v0.0.0-20241008162340-42168ba04c9d"
+    ];
+    hash = "sha256-Ui9M9/CQLEDBbDoBuejZeP2eIutlfcgYKFkGROR+sso=";
   };
+
 in
-buildGoModule {
-  pname = "caddy";
-  version = "${version}-custom-${source.date}";
-
-  src = source.src;
-  vendorHash = "sha256-088auGc7zhXj0hmll+yNwMI3QF10jdlwQT+wIAoiBLk=";
-
-  subPackages = [ "cmd/caddy" ];
-
-  ldflags = [
-    "-s" "-w"
-    "-X github.com/caddyserver/caddy/v2.CustomVersion=${version}"
-  ];
-
-  # matches upstream since v2.8.0
-  tags = [ "nobadger" ];
-
-  postInstall = ''
-    install -Dm644 ${dist}/init/caddy.service ${dist}/init/caddy-api.service -t $out/lib/systemd/system
-
-    substituteInPlace $out/lib/systemd/system/caddy.service \
-      --replace-fail "/usr/bin/caddy" "$out/bin/caddy"
-    substituteInPlace $out/lib/systemd/system/caddy-api.service \
-      --replace-fail "/usr/bin/caddy" "$out/bin/caddy"
-  '';
-
-  meta = with lib; {
-    homepage = "https://caddyserver.com";
-    description = "Fast and extensible multi-platform HTTP/1-2-3 web server with automatic HTTPS";
-    license = licenses.asl20;
-    mainProgram = "caddy";
-  };
-}
+caddy''
