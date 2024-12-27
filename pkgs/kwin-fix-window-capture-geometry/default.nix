7a79fd693624b8c5cb8c9bb6db4e73d0566880f5
oxalica <oxalicc@pm.me>
2024-12-27 14:23:11 -0500

    kwin-fix-window-capture-geometry: use upstream patch

--- pkgs/kwin-fix-window-capture-geometry/default.nix
+++ pkgs/kwin-fix-window-capture-geometry/default.nix
@@ -1,10 +1,15 @@
-# See: <https://bugs.kde.org/show_bug.cgi?id=497571>
-# TODO: Upstream this patch. Need some non-trivial rebase.
+# Bug: <https://bugs.kde.org/show_bug.cgi?id=497571>
+# Patch: https://invent.kde.org/plasma/kwin/-/commit/b6e6000d9729f740f9a17cf4d4517703a8da09fa
+# Backport: https://invent.kde.org/plasma/kwin/-/commit/4048d208f72b74f2393e6f129a1802ae447f228b
 {
   kdePackages,
+  fetchpatch,
 }:
 kdePackages.kwin.overrideAttrs (oldAttrs: {
   patches = oldAttrs.patches or [ ] ++ [
-    ./0001-Report-preferred-buffer-size-for-window-screencastin.patch
+    (fetchpatch {
+      url = "https://invent.kde.org/plasma/kwin/-/commit/4048d208f72b74f2393e6f129a1802ae447f228b.patch";
+      hash = "sha256-dWIg19tQLyHeHDXw+op7h/LrS6fW089sbO/iZF1fNyQ=";
+    })
   ];
 })
